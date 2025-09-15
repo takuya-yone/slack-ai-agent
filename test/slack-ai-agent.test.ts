8bd87da2642e0eeb122358f4605d114485183877
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2025-09-15 23:07:11 +0900

    Initialize CDK TypeScript project with essential configuration files and dependencies

---
+++ test/slack-ai-agent.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SlackAiAgent from '../lib/slack-ai-agent-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/slack-ai-agent-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SlackAiAgent.SlackAiAgentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
