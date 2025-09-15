aeb4b03ff9cf2220cde788860b30d883ee021fb1
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2025-09-15 23:54:29 +0900

    feat: add pnpm workspace configuration and enhance slack events handler with logging and tracing

--- src/lambda/slack-events-handler.ts
+++ src/lambda/slack-events-handler.ts
@@ -1,4 +1,11 @@
-export const lambdaHandler = async (event: any) => {
+import { Logger } from "@aws-lambda-powertools/logger";
+import { Tracer } from "@aws-lambda-powertools/tracer";
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+const logger = new Logger();
+const tracer = new Tracer();
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello from Slack Events Handler!" }),
